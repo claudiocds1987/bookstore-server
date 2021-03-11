582805223f877992ba21552ef9a647a5961ee785
claudiocds1987 <cla8787@gmail.com>
2021-03-11 20:45:42 -0300

    base de datos heroku

--- dist/database.js
+++ dist/database.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.pool = void 0;
 const pg_1 = require("pg");
 exports.pool = new pg_1.Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'root',
-    database: 'libreriaMVC',
+    user: 'ohcmjxflybhuxs',
+    host: 'ec2-54-90-13-87.compute-1.amazonaws.com',
+    password: '87a7776853846d79ec4826d8f7c8a7a4b7d1cb6a1b94f13d88a34a06ebe93d30',
+    database: 'd6mtnl8db77ln6',
     port: 5432
 });
--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,9 @@
 import { Pool } from 'pg'
 
 export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'root',
-    database: 'libreriaMVC',
+    user: 'ohcmjxflybhuxs',
+    host: 'ec2-54-90-13-87.compute-1.amazonaws.com',
+    password: '87a7776853846d79ec4826d8f7c8a7a4b7d1cb6a1b94f13d88a34a06ebe93d30',
+    database: 'd6mtnl8db77ln6',
     port: 5432
 })
