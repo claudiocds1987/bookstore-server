c182c4a595e5286fc2b75ab7ccd0d9cc176bdc24
claudiocds1987 <cla8787@gmail.com>
2021-03-17 18:55:43 -0300

    add ssl

--- src/database.ts
+++ src/database.ts
@@ -5,5 +5,6 @@ export const pool = new Pool({
     host: 'ec2-54-90-13-87.compute-1.amazonaws.com',
     password: '87a7776853846d79ec4826d8f7c8a7a4b7d1cb6a1b94f13d88a34a06ebe93d30',
     database: 'd6mtnl8db77ln6',
+    ssl: true,
     port: 5432
 })
