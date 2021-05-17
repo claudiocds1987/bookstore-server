431d8ef00196b490e6d596938e9aeb7bb1d50cd6
claudiocds1987 <cla8787@gmail.com>
2021-05-17 14:14:24 -0300

    borre conexion vieja

--- dist/database.js
+++ dist/database.js
@@ -4,19 +4,10 @@ exports.pool = void 0;
 const pg_1 = require("pg");
 require('dotenv').config();
 exports.pool = new pg_1.Pool({
-    user: 'ohcmjxflybhuxs',
-    host: 'ec2-54-90-13-87.compute-1.amazonaws.com',
-    password: '87a7776853846d79ec4826d8f7c8a7a4b7d1cb6a1b94f13d88a34a06ebe93d30',
-    database: 'd6mtnl8db77ln6',
-    //ssl: true,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     ssl: { "rejectUnauthorized": false },
     port: 5432
 });
-// export const pool = new Pool({
-//     user: process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     ssl: { "rejectUnauthorized": false },
-//     port: 5432
-// })
